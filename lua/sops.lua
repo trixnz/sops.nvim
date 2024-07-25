deb5718448748a847d7a49cc89918847fad82c60
Cam <me@trix.nz>
2024-07-25 21:07:44 +0200

    feat: use stdin instead of temp files

--- lua/sops.lua
+++ lua/sops.lua
@@ -3,45 +3,25 @@ local util = require("util")
 ---@class SopsModule
 local M = {}
 
-local DECRYPTED_FILE_SUFFIX = ".decrypted~"
 local SUPPORTED_FILE_FORMATS = {
   "*.yaml",
   "*.json",
 }
 
 ---@param bufnr number
-local function delete_decrypted_file(bufnr)
-  local path = vim.api.nvim_buf_get_name(bufnr)
-  local outpath = path .. DECRYPTED_FILE_SUFFIX
-
-  vim.fn.delete(outpath)
-end
-
----@param bufnr number
 local function sops_decrypt_buffer(bufnr)
   local path = vim.api.nvim_buf_get_name(bufnr)
   local cwd = vim.fs.dirname(path)
-  local decrypted_path = path .. DECRYPTED_FILE_SUFFIX
 
-  vim.system({
-    "sops",
-    "--output",
-    decrypted_path,
-    "--decrypt",
-    path,
-  }, { cwd = cwd }, function(out)
+  vim.system({ "sops", "--decrypt", path }, { cwd = cwd, text = true }, function(out)
     if out.code ~= 0 then
       vim.notify("Failed to decrypt file", vim.log.levels.WARN)
-
-      -- Make sure the decrypted file is deleted, even if it failed. We don't want to leave decrypted files around.
-      delete_decrypted_file(bufnr)
 
       return
     end
 
     vim.schedule(function()
-      local decrypted_lines = vim.fn.readfile(decrypted_path)
-      delete_decrypted_file(bufnr)
+      local decrypted_lines = vim.fn.split(out.stdout, "\n", false)
 
       -- Make this buffer writable only through our auto command
       vim.api.nvim_set_option_value("buftype", "acwrite", { buf = bufnr })
@@ -70,39 +50,53 @@ end
 local function sops_encrypt_buffer(bufnr)
   local path = vim.api.nvim_buf_get_name(bufnr)
   local cwd = vim.fs.dirname(path)
-  local decrypted_path = path .. DECRYPTED_FILE_SUFFIX
 
-  -- Write out the buffer to the decrypted file
+  -- We have to use vim.fn.jobstart here because vim.system, and vim.uv don't support /dev/stdin
+  -- https://github.com/neovim/neovim/issues/14049
+  local channel = vim.fn.jobstart({
+    "sops",
+    "--filename-override",
+    path,
+    "--output",
+    path,
+    "--encrypt",
+    "/dev/stdin",
+  }, {
+    cwd = cwd,
+    pty = true,
+  })
+
+  if channel <= 0 then
+    vim.notify("Failed to start job", vim.log.levels.WARN)
+
+    return
+  end
+
   local plaintext_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  if vim.fn.writefile(plaintext_lines, decrypted_path) ~= 0 then
-    vim.notify("Failed to write decrypted file", vim.log.levels.WARN)
+  vim.fn.chansend(channel, plaintext_lines)
+  -- Signal end of input. vim.fn.chanclose didn't seem to do the trick.
+  vim.fn.chansend(channel, "\r\004")
+
+  local code = vim.fn.jobwait({ channel }, 1000)[1]
+  if code < 0 then
+    vim.notify("Failed to run job", vim.log.levels.WARN)
 
     return
   end
 
-  -- We can't use async here, because scheduling the modified flag later seems to cause some weird undo quirks,
-  -- and we obviously can't set the modified flag until we know the outcome of running SOPs.
-  local out = vim
-    .system({
-      "sops",
-      "--filename-override",
-      path,
-      "--output",
-      path,
-      "--encrypt",
-      decrypted_path,
-    }, { cwd = cwd })
-    :wait()
-
-  delete_decrypted_file(bufnr)
-
-  if out.code ~= 0 then
-    vim.notify("Failed to encrypt file: " .. out.stderr, vim.log.levels.WARN)
+  if code ~= 0 then
+    vim.notify("SOPs failed to encrypt file: errno " .. code, vim.log.levels.WARN)
 
     return
   end
 
+  -- Mark the file as not modified
   vim.api.nvim_set_option_value("modified", false, { buf = bufnr })
+
+  -- Run BufReadPost autocmds since the buffer contents have changed
+  vim.api.nvim_exec_autocmds("BufReadPost", {
+    buffer = bufnr,
+  })
 end
 
 M.setup = function()
@@ -135,7 +129,8 @@ M.setup = function()
           -- Saving the file will always result in the SOPS-encrypted file changing, so there's no reason to save the
           -- file if the decrypted contents have not changed. Saves on false positive changes.
           if not vim.api.nvim_get_option_value("modified", { buf = bufnr }) then
-            vim.notify("Skipping sops encryption. Not modified", vim.log.levels.INFO)
+            vim.notify("Skipping sops encryption. File has not been modified", vim.log.levels.INFO)
+
             return
           end
 
