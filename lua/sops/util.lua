f6b41b7b2da0eca5475e895c828bb3ee535f7c99
Cam <me@trix.nz>
2026-09-12 18:22:28 +0200

    fix: detect dotenv files independently of Neovim filetype

--- lua/sops/util.lua
+++ lua/sops/util.lua
@@ -9,6 +9,12 @@ local SOPS_MARKER_BYTES = {
   ["env"] = "sops_mac=ENC[",
 }
 
+---@param bufnr integer
+local function is_env_file(bufnr)
+  local filename = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
+  return filename:match("%.env$") ~= nil
+end
+
 local is_conf_file = function(bufnr)
   local path = vim.api.nvim_buf_get_name(bufnr)
   local extension = vim.fn.fnamemodify(path, ":e")
@@ -22,6 +28,8 @@ M.is_sops_encrypted = function(bufnr)
   local marker
   if is_conf_file(bufnr) then
     marker = SOPS_MARKER_BYTES.binary
+  elseif is_env_file(bufnr) then
+    marker = SOPS_MARKER_BYTES.env
   else
     marker = SOPS_MARKER_BYTES[filetype]
   end
@@ -41,11 +49,11 @@ M.get_sops_format = function(bufnr)
   local sops_filetype = nvim_filetype
 
   if is_conf_file(bufnr) then -- conf does not have a standard format, so binary is assumed for maximum compatibility
-      sops_filetype = "binary"
-  elseif nvim_filetype == "env" then -- neovim refers to dotenv files as 'env'
-      sops_filetype = "dotenv" -- sops refers to dotenv files as 'dotenv'
+    sops_filetype = "binary"
+  elseif is_env_file(bufnr) or nvim_filetype == "env" then -- Neovim < 0.12 detects .env files as sh.
+    sops_filetype = "dotenv" -- sops refers to dotenv files as 'dotenv'
   elseif nvim_filetype == "toml" then -- sops doesn't support toml yet, but can be encrypted as binary for a work around
-      sops_filetype = "binary"
+    sops_filetype = "binary"
   end
   return sops_filetype
 end
